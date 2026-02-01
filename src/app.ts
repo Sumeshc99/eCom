24900ffefc980df095abe419c02673f642bd320e
Sumesh Chaure <sumeshchaure99@gmail.com>
2026-02-01 11:11:13 +0530

    aa

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,15 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 setupSwagger(app);
