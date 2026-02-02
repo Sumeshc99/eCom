e0621251cea4c2fc0ece3451feaef3d260eab4b7
Sumesh Chaure <sumeshchaure99@gmail.com>
2026-02-02 10:39:56 +0530

    aa

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
